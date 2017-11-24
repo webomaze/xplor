cd650fadf84942306ff157f1a66163b063ee60d9
deepikabiswas <deepika.biswas@webomaze.com>
2017-11-24 13:11:00 +0100

    all files from root is added now

--- app/etc/env.php
+++ app/etc/env.php
@@ -1,7 +1,61 @@
 <?php
 return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_xplor8',
+  ),
+  'crypt' => 
+  array (
+    'key' => '854b363c5ba3ece3132477a5978099e4',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'd3m0web_xplore',
+        'username' => 'd3m0web_xplore',
+        'password' => '#L#=b*V0xoTQ',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
   'cache_types' => 
   array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'amasty_shopby' => 1,
     'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Mon, 25 Sep 2017 07:00:31 +0000',
   ),
 );
